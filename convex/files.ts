5816af59a7d4a3718f940c91048a23464822bce2
maaz2022 <maaznazeer098@gmail.com>
2024-07-18 15:26:27 +0500

    initial setup

--- convex/files.ts
+++ convex/files.ts
@@ -1,3 +1,27 @@
 
-import { mutation } from "./_generated/server"
-export const createFile = mutation({})
+import { ConvexError, v } from "convex/values"
+import { mutation, query } from "./_generated/server"
+export const createFile = mutation({
+    args: {
+        name: v.string(),
+    },
+    async handler(ctx, args){   
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new ConvexError("go and logged in first")
+        }
+        await ctx.db.insert("files",{
+            name: args.name,
+        })
+     
+    },
+ 
+})
+
+export const getFiles = query({
+    args: {},
+    async handler(ctx, args){
+        return ctx.db.query("files").collect();
+    }
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
 import Image from "next/image";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const files = useQuery(api.files.getFiles);
+ const createFile = useMutation(api.files.createFile);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Hello bro</Button>
+      <SignInButton/>
+      {files?.map((file) =>{
+      return <div key={file._id}>{file.name}</div>
+      })}
+      <Button onClick={()=>{
+        createFile({
+          name:"hello world"
+        })
+      }}>Click</Button>
     </main>
   );
 }
